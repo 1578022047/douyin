4bd5f65502e5a084aa1a7ae65803fe7bbb4e7993
liuyishoua <1813120647@qq.com>
2020-01-06 14:29:52 +0800

    这些部分是我改的，你用的时候先导入项目，实现更改同步，再来开发！！！

--- app/src/main/java/com/example/dou/fragment/IndexFragment.java
+++ app/src/main/java/com/example/dou/fragment/IndexFragment.java
@@ -1,5 +1,6 @@
 package com.example.dou.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,17 +11,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import com.cjt2325.cameralibrary.listener.ClickListener;
 import com.example.dou.R;
 import com.example.dou.adapter.FragmentAdapter;
+import com.example.dou.ui.activity.UserActivity;
 import com.example.dou.viewpage.AddMethodFragment;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class IndexFragment extends Fragment {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class IndexFragment extends Fragment implements View.OnClickListener {
     private FragmentAdapter adapter;
     private List<AddMethodFragment> list;
+    private CircleImageView user_img;
     ViewPager viewPager;
     TabLayout tabLayout;
 
@@ -37,12 +43,20 @@ public class IndexFragment extends Fragment {
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_index,container,false);
         viewPager=view.findViewById(R.id.viewPager);
+        user_img = view.findViewById(R.id.user_image);
+
+//        初始化监听
+        initListener();
+//        初始化adapter信息
         init();
         tabLayout = view.findViewById(R.id.index_tablayout);
         tabLayout.setupWithViewPager(viewPager);
         return view;
     }
 
+    private void initListener() {
+        user_img.setOnClickListener(this);
+    }
 
     private void init() {
         adapter=new FragmentAdapter(getActivity().getSupportFragmentManager(),list);
@@ -50,4 +64,17 @@ public class IndexFragment extends Fragment {
         viewPager.setCurrentItem(1);
     }
 
+
+    @Override
+    public void onClick(final View v) {
+        switch (v.getId()){
+            case R.id.user_image:
+                Intent intent = new Intent(getActivity(), UserActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+    }
 }
---
+++ app/src/main/java/com/example/dou/ui/activity/UserActivity.java
@@ -0,0 +1,95 @@
+package com.example.dou.ui.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.dou.R;
+import com.example.dou.viewpage.ViewPagerAdapter;
+import com.example.dou.viewpage.ViewPagerVideoFragment;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class UserActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private CollapsingToolbarLayout toolbarLayout;
+    private List<ViewPagerVideoFragment> viewPagerVideoFragments = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        init();
+        initListener();
+
+//        toolbar等操作
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        toolbarLayout.setTitle("标题可以设置成自己想要的标题");
+
+//        viewpager及tablayout等操作
+        viewPagerVideoFragments.add(new ViewPagerVideoFragment("作品","neirong1"));
+        viewPagerVideoFragments.add(new ViewPagerVideoFragment("动态","neirong2"));
+        viewPagerVideoFragments.add(new ViewPagerVideoFragment("喜欢","neirong3"));
+        //        设置viewpager适配器，以及添加适配器数据
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),viewPagerVideoFragments);
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(1);
+        viewPager.setOffscreenPageLimit(3);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void initListener() {
+        toolbar = findViewById(R.id.toolbar);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        toolbarLayout = findViewById(R.id.collaps);
+    }
+
+    private void init() {
+    }
+
+    @Override
+    public void onClick(final View v) {
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.user_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull final MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.user_info:
+                Intent intent = new Intent(this,UserInfoActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
