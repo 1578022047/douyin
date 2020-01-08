d205baeb20f7e9a9465e38ea27d3229acf39a9fd
15780 <1578022045@qq.com>
2020-01-08 23:42:05 +0800

    11

---
+++ app/src/main/java/com/example/dou/interfaces/OnViewPagerListener.java
@@ -0,0 +1,12 @@
+package com.example.dou.interfaces;
+
+import android.view.View;
+
+public interface OnViewPagerListener {
+
+    void onInitComplete(View view);
+
+    void onPageRelease(boolean isNext, int position, View view);
+
+    void onPageSelected(int position, boolean isBottom, View view);
+}
